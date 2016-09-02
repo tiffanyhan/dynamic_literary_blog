812ef04b1e9197c4c778342f6c1da9829eb092e6
Tiffany Han <hantiffany@gmail.com>
2016-09-02 10:37:48 -0700

    add code comments and navigation through routes

--- main.py
+++ main.py
@@ -35,66 +35,277 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape = True)
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+	'''
+	checks that a valid username was entered
+	'''
+	return username and USER_RE.match(username)
+
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+	'''
+	checks that a valid password was entered
+	'''
+	return password and PASSWORD_RE.match(password)
+
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+	'''
+	checks that either an email was not entered or
+	if it was, that a valid one was entered
+	'''
+	return not email or EMAIL_RE.match(email)
+
+
+# PASSWORD HASHING
+def make_salt():
+	'''
+	makes a salt to be used for hashing passwords
+	'''
+	return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt=''):
+	'''
+	hashes a password, either with a new salt or
+	with an already existing one
+	'''
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s|%s' % (salt, h)
+
+def valid_pw(name, password, h):
+	'''
+	takes the salt from the user's hashed pw in the db,
+	and ensures that hashing the entered pw with that salt
+	gives the hashed pw in the db
+	'''
+	salt = h.split('|')[0]
+	return h == make_pw_hash(name, password, salt)
+
+
+def blog_key(name = 'default'):
+	'''
+	returns a key to be used as the parent for each
+	submission instance created
+	'''
+	return db.Key.from_path('/', name)
+
+
+class User(db.Model):
+	'''
+	class used to create the user instances to be
+	inserted into and retrieved from the db
+	'''
+	username = db.StringProperty(required = True)
+	pw_hash = db.StringProperty(required = True)
+	email = db.StringProperty()
+
+	@classmethod
+	def by_id(cls, uid):
+		'''
+		retrieves a user instance from the db using the
+		cookie's user-id value
+		'''
+		return User.get_by_id(uid, parent=users_key())
+
+	@classmethod
+	def by_name(cls, name):
+		'''
+		retrieves a user instance from the db using the
+		username that was entered
+		'''
+		u = User.all().filter('username =', name).get()
+		return u
+
+	@classmethod
+	def register(cls, name, pw, email = None):
+		'''
+		takes all user information, makes a pw hash, then
+		creates a new user instance with the hashed pw
+		'''
+		pw_hash = make_pw_hash(name, pw)
+		return User(parent = users_key(),
+					username = name,
+					pw_hash = pw_hash,
+					email = email)
+
+	@classmethod
+	def login(cls, name, pw):
+		'''
+		if this user exists in the db, and their pw
+		is a valid one, then return that user instance
+		'''
+		u = cls.by_name(name)
+		if u and valid_pw(name, pw, u.pw_hash):
+			return u
+
+
+class Submission(db.Model):
+	'''
+	class used to create the submission instances to be
+	inserted into and retrieved from the db
+	'''
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
+	def render(self):
+		'''
+		method that renders the submission using the submission.html
+		template, correctly rendering line breaks in html
+		'''
+		print('render called')
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str('submission.html', submission=self)
+
+
+def users_key(name = 'default'):
+	'''
+	returns a key to be used as the parent for each
+	user instance created
+	'''
+	return db.Key.from_path('/', name)
+
+
+# COOKIES
+def hash_str(s):
+	'''
+	given some input, creates a hashed output.
+	used for making secure cookie values
+	'''
+	return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+	'''
+	makes a secure cookie value
+	'''
+	return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+	'''
+	takes the unhashed part of the cookie.  if hashing it
+	matches the entire cookie value, then the unhashed
+	part of the cookie is returned
+	'''
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+
+
 def render_str(template, **params):
+	'''
+	a global version of render_str that is available
+	to functions that don't inherit from Handler
+	'''
 	print('render string called')
 	t = jinja_env.get_template(template)
 	return t.render(params)
 
+
 class Handler(webapp2.RequestHandler):
+	'''
+	class that all our handlers will inherit from.
+	contains convenience functions that all handlers
+	will have access to.
+	'''
+
 	def write(self, *a, **kw):
+		'''
+		a convenience function for rendering templates
+		'''
 		self.response.write(*a, **kw)
 
 	def render_str(self, template, **params):
+		'''
+		a convenience function for rendering templates
+		'''
 		t = jinja_env.get_template(template)
 		return t.render(params)
 
 	def render(self, template, **kw):
+		'''
+		renders a given template with the provided parameters
+		'''
 		self.write(self.render_str(template, **kw))
 
 	def set_secure_cookie(self, name, val):
+		'''
+		makes a secure cookie value, adds the cookie
+		to the response headers
+		'''
 		cookie_val = make_secure_val(val)
 		self.response.headers.add_header(
 			'Set-Cookie',
 			'%s=%s; Path=/' % (name, cookie_val))
 
 	def read_secure_cookie(self, name):
+		'''
+		if a value exists for a given cookie, check if the
+		value is valid.  if it is, return that value.
+		'''
 		cookie_val = self.request.cookies.get(name)
 		return cookie_val and check_secure_val(cookie_val)
 
 	def login(self, user):
+		'''
+		given a user instance, get its key identifier and
+		use this to set a secure cookie value
+		'''
 		self.set_secure_cookie('user-id', str(user.key().id()))
 
 	def logout(self):
+		'''
+		clears the cookie from the response headers
+		'''
 		self.response.headers.add_header('Set-Cookie', 'user-id=; Path=/')
 
 	def initialize(self, *a, **kw):
+		'''
+		this function is automatically invoked whenever a user
+		visits any page.
+
+		if a secure value exists for the user-id cookie, and
+		the associated user instance in the db exists, then
+		self.user is set to that user instance
+
+		'''
 		webapp2.RequestHandler.initialize(self, *a, **kw)
 		uid = self.read_secure_cookie('user-id')
 		self.user = uid and User.by_id(int(uid))
 
-def blog_key(name = 'default'):
-	return db.Key.from_path('/', name)
-
-class Submission(db.Model):
-	subject = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-
-	def render(self):
-		print('render called')
-		self._render_text = self.content.replace('\n', '<br>')
-		return render_str('submission.html', submission=self)
 
 class MainHandler(Handler):
 	def get(self):
+		'''
+		show all submissions on the main page
+		'''
 		submissions = db.GqlQuery("SELECT * from Submission ORDER BY created DESC limit 10")
 		self.render("main.html", submissions=submissions)
 
 class NewHandler(Handler):
 	def get(self):
-		self.render("new.html")
+		'''
+		renders the form to submit a new blog entry
+		'''
+		if self.user:
+			self.render("new.html")
+		else:
+			self.redirect('/login')
 
 	def post(self):
+		'''
+		creates a new submission instance and inserts it into the db.  then redirects
+		the user to the blog entry permalink page.  both subject and content are required
+		to make a blog entry.
+		'''
+		if not self.user:
+			self.redirect('/login')
+
 		subject = self.request.get("subject")
 		content = self.request.get("content")
 
@@ -111,6 +322,13 @@ class NewHandler(Handler):
 
 class SubmissionHandler(Handler):
 	def get(self, submission_id):
+		'''
+		using the submission_id passed in as a parameter in the URL, gets the
+		associated submission instance in the db and renders the permalink page
+		with it
+
+		if the associated submission instance in the db DNE, returns a 404 error
+		'''
 		key = db.Key.from_path('Submission', int(submission_id), parent=blog_key())
 		submission = db.get(key)
 
@@ -120,81 +338,28 @@ class SubmissionHandler(Handler):
 
 		self.render("permalink.html", submission=submission)
 
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-	return username and USER_RE.match(username)
-
-PASSWORD_RE = re.compile(r"^.{3,20}$")
-def valid_password(password):
-	return password and PASSWORD_RE.match(password)
-
-EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
-def valid_email(email):
-	return not email or EMAIL_RE.match(email)
-
-# password stuff
-def make_salt():
-	return ''.join(random.choice(string.letters) for x in xrange(5))
-
-def make_pw_hash(name, pw, salt=''):
-	if not salt:
-		salt = make_salt()
-	h = hashlib.sha256(name + pw + salt).hexdigest()
-	return '%s|%s' % (salt, h)
-
-def valid_pw(name, password, h):
-	salt = h.split('|')[0]
-	return h == make_pw_hash(name, password, salt)
-
-# cookie stuff
-def hash_str(s):
-	return hmac.new(SECRET, s).hexdigest()
-
-def make_secure_val(s):
-	return "%s|%s" % (s, hash_str(s))
-
-def check_secure_val(h):
-	val = h.split('|')[0]
-	if h == make_secure_val(val):
-		return val
-
-# user class for database
-def users_key(name = 'default'):
-	return db.Key.from_path('/', name)
-
-class User(db.Model):
-	username = db.StringProperty(required = True)
-	pw_hash = db.StringProperty(required = True)
-	email = db.StringProperty()
-
-	@classmethod
-	def by_id(cls, uid):
-		return User.get_by_id(uid, parent=users_key())
-
-	@classmethod
-	def by_name(cls, name):
-		u = User.all().filter('username =', name).get()
-		return u
-
-	@classmethod
-	def register(cls, name, pw, email = None):
-		pw_hash = make_pw_hash(name, pw)
-		return User(parent = users_key(),
-					username = name,
-					pw_hash = pw_hash,
-					email = email)
-
-	@classmethod
-	def login(cls, name, pw):
-		u = cls.by_name(name)
-		if u and valid_pw(name, pw, u.pw_hash):
-			return u
 
 class SignUpHandler(Handler):
 	def get(self):
-		self.render("signup.html")
+		'''
+		renders the form to register
+		'''
+		if self.user:
+			self.redirect('/logout')
+		else:
+			self.render("signup.html")
 
 	def post(self):
+		'''
+		creates a new user instance and inserts it into the db.
+		logs the user in, then redirects the user to the welcome page.
+
+		if there are any errors, render the signup form again with
+		the appropriate saved data and error messages
+		'''
+		if self.user:
+			self.redirect('/logout')
+
 		have_error = False
 
 		self.username = self.request.get("username").encode("latin-1")
@@ -234,24 +399,47 @@ class SignUpHandler(Handler):
 			self.login(user)
 			self.redirect('/welcome')
 
+
 class WelcomeHandler(Handler):
 	def get(self):
+		'''
+		if the user is logged in, render the welcome page.
+		if not, render the signup page.
+		'''
 		if self.user:
 			self.render("welcome.html", user=self.user)
 		else:
-			self.redirect("/signup")
+			self.redirect("/login")
+
 
 class LogInHandler(Handler):
 	def get(self):
+		'''
+		render the form to login
+		'''
+		if self.user:
+			self.redirect('/logout')
+
 		self.render('login.html')
 
 	def post(self):
+		'''
+		check the information entered against information stored
+		in the db.  if all is well, log the user in and redirect
+		them to the welcome page.
+
+		if not, render the login form again with an error message
+		'''
+		if self.user:
+			self.redirect('/logout')
+
 		have_error = False
 
 		username = self.request.get("username").encode("latin-1")
 		password = self.request.get("password").encode("latin-1")
 
 		user = User.login(username, password)
+
 		if user:
 			self.login(user)
 			self.redirect('/welcome')
@@ -259,11 +447,25 @@ class LogInHandler(Handler):
 			error = 'Invalid login'
 			self.render('login.html', error=error)
 
+
 class LogOutHandler(Handler):
 	def get(self):
-		self.logout()
-		self.redirect('/signup')
+		if self.user:
+			self.render('logout.html')
+		else:
+			self.redirect('/')
 
+	def post(self):
+		'''
+		log the user out and redirect them to the signup page
+		'''
+		if not self.user:
+			self.redirect('/')
+
+		self.logout()
+		self.redirect('/')
+
+# ALL ROUTES AND ASSOCIATED HANDLERS
 app = webapp2.WSGIApplication([
 	('/', MainHandler),
 	('/newpost', NewHandler),
